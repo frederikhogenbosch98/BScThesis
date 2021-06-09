a89458a0f3e17884b51c0f2a5b780e1fb8b4af7c
frederikhogenbosch98 <fhogenbosch@icloud.com>
2021-06-09 15:12:58 +0200

    cleanup main.f90

--- DGPT_DL/main.f90
+++ DGPT_DL/main.f90
@@ -52,8 +52,8 @@ integer count_rate, count_max
 double precision time_init, time_final, elapsed_time
 
 
-! Set the energy domain and discretization (uniform)
 
+! Set the energy domain and discretization (uniform)
 E_max = 201.0_dp
 E_min = 1.0_dp
 dE = (E_max-E_min) / real(no_grps,dp)
@@ -61,14 +61,16 @@ E_bounds(1) = E_max
 do gr=1,no_grps
   E_bounds(gr+1) = E_bounds(gr) - dE(gr)
 enddo
-!print *, E_bounds
-dx = x_max / no_steps
-! Init
 
+dx = x_max / no_steps
+
+
+! Init phi
 phi     = 0.0_dp
 phi_old = 0.0_dp
-! Gaussian boundary condition
 
+
+! Gaussian boundary condition
 A     = 1.0_dp
 mu    = 100.0_dp
 sigma = 1.0_dp / sqrt(2.0_dp)
@@ -87,7 +89,7 @@ dE_bounded = dE(1:int_size)
 call calc_M(M_band,kl_M,ku_M,dE_bounded)
 
 
-
+! Init E and phi bounds
 call det_bounds(E_bounds, dE, phi, E_bounded, phi_bounded, n_max, int_size)
 phi_bounded_old = phi_bounded
 phi_bounded_un = phi_bounded
@@ -99,40 +101,38 @@ E_bounded_old = E_bounded
 !Timing
 call system_clock(count_0, count_rate, count_max)
 time_init=count_0*1.0/count_rate
-!print *, phi_bounded
 
-
+! Init interval
 interval = n_max
+
+
 ! Stepping
 do step=1,2000
 
+  ! Define update boundary
   if (step<1500) then
       phi_boundary = 0.001_dp
   else
       phi_boundary = 0.0023_dp
   endif
-  ! Construct G matrix with CSD and straggling
+
+  ! Update E and phi bounds
   if (phi_bounded(int_size*2)> phi_boundary) then
     print *, 'updated at step: ', step
     call update_bounds(E_bounds, phi_old, E_bounded_old, dE, phi_bounded_old, E_bounded, phi_bounded, interval, step, n_max, int_size,no_steps, updated, phi_proj, new_interval)    
     updated = step
     phi_non_cn = phi_bounded
     interval = new_interval
-    do gr=int_size,1,-1
-        phi_high = phi_bounded(2*(gr-1)+1) + phi_bounded(2*(gr-1)+2)
-!        write(12,*) phi_high
-        !print *, phi_high
-    enddo
-  endif
-    if(step==2000) then
-      do grdos=int_size,1,-1
-        phi_high = phi_bounded(2*(grdos-1)+1) + phi_bounded(2*(grdos-1)+2)
-        !write(14,*) phi_high
-        !print *, phi_high
-      enddo
-      !print *, step
   endif
 
+  ! Plot phi at step
+    if(step==2000) then
+      do gr=int_size,1,-1
+        phi_high = phi_bounded(2*(gr-1)+1) + phi_bounded(2*(gr-1)+2)
+      enddo
+  endif
+
+  ! Plot phi every 100 steps
   if (mod(step,100)==0) then
       !print *, step
       call project_phi(interval, int_size, phi_bounded_old, phi_proj)
@@ -141,39 +141,45 @@ do step=1,2000
       enddo
         write(14,*) phi_plot
   endif
-    phi_bounded_old = phi_bounded
-  call build_G_band(size(phi_bounded),E_bounded,dE_bounded,G_band,kl_G,ku_G)
-  ! Do single CN step
 
+  phi_bounded_old = phi_bounded
+  
+  ! Construct G matrix
+  call build_G_band(size(phi_bounded),E_bounded,dE_bounded,G_band,kl_G,ku_G)
+
+  ! Do single CN step
   call CN_1step(size(phi_bounded),M_band,kl_M,ku_M,G_band,kl_G,ku_G,phi_bounded_old,phi_bounded,dx)
 
-  ! Time copy
+  ! Copy
   phi_old = phi
   phi_bounded_old = phi_bounded
 enddo
-print *, E_bounded
+
+! End system clock
 call system_clock(count_1, count_rate, count_max)
 time_final = count_1*1.0/count_rate
 elapsed_time = time_final-time_init
 
 print *,"elasped time: ",elapsed_time
 
-! Plot flux
+! Project phi
 call project_phi(no_grps-int_size, int_size, phi_bounded_old, phi_proj)
 
+! Plot flux
 do gr=no_grps,1,-1
     E_low  = E_bounds(gr+1)
     E_high = E_bounds(gr)
-    phi_low  = phi_proj(2*(gr-1)+1) + phi_proj(2*(gr-1)+2)
-    !print *, phi_low
-    write(12,*) phi_low
-    !phi_high = phi_proj(2*(gr-1)+1) + phi_proj(2*(gr-1)+2)
+    phi_low  = phi_proj(2*(gr-1)+1) - phi_proj(2*(gr-1)+2)
+    phi_high = phi_proj(2*(gr-1)+1) + phi_proj(2*(gr-1)+2)
 enddo
 
-do grdos=int_size,1,-1
-    phi_high = phi_bounded_un(2*(grdos-1)+1) + phi_bounded_un(2*(grdos-1)+2)
-    !print *, phi_high
+
+do gr=int_size,1,-1
+    phi_low = phi_bounded_un(2*(gr-1)+1) - phi_bounded_un(2*(gr-1)+2)
+    phi_high = phi_bounded_un(2*(gr-1)+1) + phi_bounded_un(2*(gr-1)+2)
 enddo
+
+
 close(14)
 close(12)
 
